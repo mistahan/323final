693839487f724c3eb2a997fe5a485a840fc54319
mistahan <han.alex123@gmail.com>
2016-12-03 20:59:53 -0800

    Update 323finalproj.sql

--- 323finalproj.sql
+++ 323finalproj.sql
@@ -12,8 +12,7 @@
  */
 CREATE TABLE customers (
     cusID       INTEGER         NOT NULL,
-    FName       VARCHAR(20)     NOT NULL,
-    lname       VARCHAR(20)     NOT NULL, 
+    Name        VARCHAR(20)     NOT NULL,
     PHONE       VARCHAR(12)     NOT NULL,
     email       VARCHAR(40)     NOT NULL,
     
@@ -89,12 +88,10 @@ CREATE TABLE premier(
 
 CREATE TABLE corporation(
     address      VARCHAR(40)  NOT NULL,
+    typeofAdd    VARCHAR(10)  NOT NULL,
     cusID        INTEGER      NOT NULL,
     CONSTRAINT corp_cus_fk  FOREIGN KEY (cusID) REFERENCES customers (cusID)  
 );
-
-
-
 
 CREATE TABLE individual(
     cusID       INTEGER       NOT NULL,
@@ -240,17 +237,42 @@ CREATE TABLE repairInstance(
     CONSTRAINT repInst_maitnence_fk FOREIGN KEY (packageID) REFERENCES maitenancePack
 );
 
+
+
 /*INSERT VALUES FOR DATA*/
-INSERT INTO CUSTOMERS VALUES(001,'Alex', 'Han', '714-444-3211', 'han@alex.com'); 
+INSERT INTO CUSTOMERS VALUES(001,'Alex Han', '714-444-3211', 'han@alex.com');
+INSERT INTO CUSTOMERS VALUES(002,'blex Han', '714-628-2619', 'han@blex.com');
+INSERT INTO CUSTOMERS VALUES(003,'Tyren Han', '714-996-3321', 'han@tyren.com');
+INSERT INTO CUSTOMERS VALUES(004,'Brian Han', '714-831-3211', 'han@brian.com');
+INSERT INTO CUSTOMERS VALUES(005,'Alex Riddle', '714-210-5515', 'riddle@alex.com');
+INSERT INTO CUSTOMERS VALUES(006,'CARS CORP', '562-444-9880', 'employee@cars.corp');
+
+INSERT INTO INDIVIDUAL VALUES(001,'1312 Travel lane');
+INSERT INTO INDIVIDUAL VALUES(002,'906 sycamore street lane');
+INSERT INTO INDIVIDUAL VALUES(003,'132 BIG WORM ave');
+INSERT INTO INDIVIDUAL VALUES(004,'4456 crenshaw blvd');
+INSERT INTO INDIVIDUAL VALUES(005,'2368 Crippling Dr');
+
+INSERT INTO existingCustomer VALUES (001,'2016-12-03', '03:22:00');
+INSERT INTO existingCustomer VALUES (002,'2015-06-05', '06:06:00');
+INSERT INTO existingCustomer VALUES (003,'2001-07-12', '15:09:00');
+INSERT INTO existingCustomer VALUES (004,'2013-01-20', '12:13:00');
+INSERT INTO existingCustomer VALUES (006,'1999-11-25', '09:10:00');
+
+INSERT INTO prospective VALUES (005,'emailed');
 
 
 /*VEWIS*/
 
 /*QUERRIES*/
 SELECT * From customers;
+SELECT * From INDIVIDUAL;
+SELECT * From existingCustomer;
+SELECT * From prospective;
 
--- USED ONLY TO DROP TABLES 
-/*ALTER TABLE TO DROP CONSTRAINTS
+
+/*
+
 ALTER TABLE existingCustomer
     DROP CONSTRAINT excus_cus_fk;
 ALTER TABLE prospective
@@ -294,7 +316,6 @@ ALTER TABLE skillMechanic
 ALTER TABLE CERTIFICATION
     DROP CONSTRAINT cert_mech_fk;
 
-/*DROP TABLES TO RUN QUERRIES FLAWLESSLY
 DROP Table Customers;
 DROP Table existingCustomer;
 DROP Table prospective;
@@ -319,5 +340,4 @@ DROP TABLE skillMechanic;
 DROP TABLE SKILL;
 DROP TABLE SKILLREPAIR;
 DROP TABLE CERTIFICATION;
-
 
