95fa1c361158c31c8c7d814ae2aee8ef16f95c6a
mistahan <han.alex123@gmail.com>
2016-12-04 11:00:03 -0800

    Update 323finalproj.sql

--- 323finalproj.sql
+++ 323finalproj.sql
@@ -7,7 +7,6 @@
  *   do we want to carry the cus id all the way down as far as the order line?
  *   
  *   MAITNENCE package ????? issues DOES IT REQUIRE A MANY TO MANY OF EACH ITEM 
- =
  *   
  */
 CREATE TABLE customers (
@@ -233,44 +232,6 @@ CREATE TABLE repairInstance(
     CONSTRAINT repInst_repItem_fk FOREIGN KEY (namItem) REFERENCES repairItems,
     CONSTRAINT repInst_maitnence_fk FOREIGN KEY (packageID) REFERENCES maitenancePack
 );
-
-/*
-
-/*INSERT VALUES FOR DATA*/
-INSERT INTO CUSTOMERS VALUES(001,'Alex Han', '714-444-3211', 'han@alex.com');
-INSERT INTO CUSTOMERS VALUES(002,'blex Han', '714-628-2619', 'han@blex.com');
-INSERT INTO CUSTOMERS VALUES(003,'Tyren Han', '714-996-3321', 'han@tyren.com');
-INSERT INTO CUSTOMERS VALUES(004,'Brian Han', '714-831-3211', 'han@brian.com');
-INSERT INTO CUSTOMERS VALUES(005,'Alex Riddle', '714-210-5515', 'riddle@alex.com');
-INSERT INTO CUSTOMERS VALUES(006,'CARS CORP', '562-444-9880', 'employee@cars.corp');
-
-INSERT INTO INDIVIDUAL VALUES(001,'1312 Travel lane');
-INSERT INTO INDIVIDUAL VALUES(002,'906 sycamore street lane');
-INSERT INTO INDIVIDUAL VALUES(003,'132 BIG WORM ave');
-INSERT INTO INDIVIDUAL VALUES(004,'4456 crenshaw blvd');
-INSERT INTO INDIVIDUAL VALUES(005,'2368 Crippling Dr');
-
-INSERT INTO existingCustomer VALUES (001,'2016-12-03', '03:22:00');
-INSERT INTO existingCustomer VALUES (002,'2015-06-05', '06:06:00');
-INSERT INTO existingCustomer VALUES (003,'2001-07-12', '15:09:00');
-INSERT INTO existingCustomer VALUES (004,'2013-01-20', '12:13:00');
-INSERT INTO existingCustomer VALUES (006,'1999-11-25', '09:10:00');
-
-INSERT INTO prospective VALUES (005,'emailed');
-
-INSERT INTO SPECIAL VALUES ('50% OFF SERVICE','2016-12-31');
-INSERT INTO SPECIAL VALUES ('50% OFF PACKAGE','2016-12-31');
-INSERT INTO SPECIAL VALUES ('30% OFF AIR FILTER','2016-12-31');
-INSERT INTO SPECIAL VALUES ('12345678901234567890','2016-12-31');
-
-INSERT INTO specialInstance VALUES (006,'2016-04-12','30% OFF AIR FILTER','2016-12-31'); 
-/*VEWIS*/
-
-/*QUERRIES*/
-SELECT * From customers;
-SELECT * From INDIVIDUAL;
-SELECT * From existingCustomer;
-SELECT * From prospective;
 
 
 /*
