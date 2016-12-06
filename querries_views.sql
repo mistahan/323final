e6c5ebd827433202f4cd9b967007370691a1eb41
mistahan <han.alex123@gmail.com>
2016-12-05 23:52:56 -0800

    Add files via upload

--- querries_views.sql
+++ querries_views.sql
@@ -45,9 +45,11 @@ SELECT * FROM SKILLREPAIR;
 -- we will need to adjust them in the future
 
 --1
-    CREATE VIEW Customer_v AS SELECT * From customers;
+    Select existingCustomer.CUSID, customers."NAME",customers.PHONE,customers.EMAIL From existingCustomer
+INNER JOIN customers
+On customers.cusID = existingCustomer.CUSID;
 --2
-    CREATE VIEW Customer_addresses_v AS SELECT * From customers;
+
 --3
     CREATE VIEW Mechanic_mentor_v  AS 
         SELECT E.FNAME as"fmento", E.lName AS "mento",M.FNAME AS "fmente",
@@ -58,31 +60,54 @@ SELECT * FROM SKILLREPAIR;
           ON E.empID = S.empID
         ORDER BY E.lName;
 --4
-    CREATE VIEW Premier_profits_v  AS SELECT * From customers;
+
+   
 --5
     CREATE VIEW Prospective_resurrection_v  AS SELECT * From customers;
 
 /*quierries*/
 --1
-  SELECT E.lName AS "mento", M.lName AS "mente"
-        FROM mechanic E LEFT OUTER JOIN 
-          (mentorship S INNER JOIN mechanic M 
-            ON S.mentorID = M.empID)
-          ON E.empID = S.empID
-        ORDER BY E.lName;
+SELECT Name, phone, email, mailingAdd AS "ADDRESS",'Home' AS "Type of Address", 'Individual' AS "Type of Customer"  FROM customers NATURAL JOIN individual
+UNION
+SELECT Name, phone, email, address AS "ADDRESS", typeOfAdd AS "Type of Address",'Corporation' AS "Type of Customer" FROM customers NATURAL JOIN corporation NATURAL JOIN ADDRESS;
 --2
-
+select customers."NAME", sum(repairInstance.PRICE) from customers
+natural join (vehicle
+natural join orders
+natural join orderline
+natural join repairItems
+natural join repairInstance)
+group by customers."NAME";
 --3
 
 --4
+SELECT DISTINCT fname, lname, COUNT(skillName)
+From (employee NATURAL JOIN mechanic 
+               NATURAL JOIN skillMechanic 
+               NATURAL JOIN skill)
+GROUP BY fname, lname
+HAVING COUNT(skillName) > 2;
+
 
 --5
 
 --6
+SELECT t1.packageName, "Price per Package",namItem,"Price per Item"
+FROM(
+(SELECT packageName, namItem, price AS "Price per Item"
+FROM repairItems natural join (maitenancePack left outer join repairInstance using (packageID))) t1
+LEFT OUTER JOIN
+(SELECT packageName, SUM(price) AS "Price per Package" FROM ((maitenancePack left outer join repairInstance using (packageID)))
+GROUP BY packageName) t2
+ON t1.packageName = t2.packageName);
 
 --7
 
 --8
+select customers."NAME", steady.LOYALPOINT from steady
+inner join customers
+on steady.CUSID = customers.CUSID
+order by loyalpoint DESC;
 
 --9
 
@@ -99,6 +124,12 @@ SELECT * FROM SKILLREPAIR;
 --15
 SELECT empID, fname, lname from employee natural join technician natural join 
         mechanic;
+--or
+SELECT employee.EMPID, employee.fname, employee.lname from employee 
+inner join technician
+on employee.EMPID = technician.EMPID
+inner join mechanic
+on technician.EMPID = mechanic.EMPID;
 
 
 /*team querries*/
